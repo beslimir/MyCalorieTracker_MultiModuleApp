ff9970abd40c303f45b3faf1263f61633c852886
beslimir <marin.beslo@gmail.com>
2022-01-31 09:26:53 +0100

    add navigation

--- app/src/main/java/com/example/mycalorietracker/MainActivity.kt
+++ app/src/main/java/com/example/mycalorietracker/MainActivity.kt
@@ -3,21 +3,54 @@ package com.example.mycalorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.core.navigation.Route
+import com.example.mycalorietracker.navigation.navigate
 import com.example.mycalorietracker.ui.theme.MyCalorieTrackerTheme
+import com.example.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyCalorieTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.WELCOME
+                ) {
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE) {
 
+                    }
+                    composable(Route.GENDER) {
+
+                    }
+                    composable(Route.HEIGHT) {
+
+                    }
+                    composable(Route.WEIGHT) {
+
+                    }
+                    composable(Route.NUTRIENT_GOAL) {
+
+                    }
+                    composable(Route.ACTIVITY) {
+
+                    }
+                    composable(Route.GOAL) {
+
+                    }
+                    composable(Route.TRACKER_OVERVIEW) {
+
+                    }
+                    composable(Route.SEARCH) {
+
+                    }
                 }
             }
         }
