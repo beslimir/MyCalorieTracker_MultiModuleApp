3b4e5d8dfc206c376efcd53270b0d73b6f6d30c7
beslimir <marin.beslo@gmail.com>
2022-02-04 10:35:20 +0100

    small changes

--- tracker/tracker_domain/src/main/java/com/example/tracker_domain/repository/TrackerRepository.kt
+++ tracker/tracker_domain/src/main/java/com/example/tracker_domain/repository/TrackerRepository.kt
@@ -12,8 +12,11 @@ interface TrackerRepository {
         page: Int,
         pageSize: Int
     ): Result<List<TrackableFood>>
+
     suspend fun insertTrackedFood(food: TrackedFood)
+
     suspend fun deleteTrackedFood(food: TrackedFood)
-    fun getFoodsForDate(localDate: LocalDate): Flow<List<TrackedFood>>
+
+    fun getFoodForDate(localDate: LocalDate): Flow<List<TrackedFood>>
 
 }
